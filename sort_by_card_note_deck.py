5dac85e8eeaec0257b6d8ef468a90eabcb50f9dd
Andreas <519178+schmidhauser@users.noreply.github.com>
2019-12-16 03:16:49 -0800

    License added

--- sort_by_card_note_deck.py
+++ sort_by_card_note_deck.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+# Copyright: (c) 2014 Teemu Pudas, (c) 2019 Andreas U. Schmidhauser.
+# Please report issues to <https://github.com/schmidhauser>.
+# License: GNU AGPLv3 <https://www.gnu.org/licenses/agpl.html>.
 
 """
 SORT BY CARD TYPE, NOTE TYPE, OR DECK NAME
